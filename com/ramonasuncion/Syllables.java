a1de7627f943acf2da5a50f7c994172dfcaa9807
Ramon Asuncion <asuncionbatista@gmail.com>
2022-01-18 21:31:34 -0500

    Remove multiple if statements to make it cleaner.

--- com/ramonasuncion/Syllables.java
+++ com/ramonasuncion/Syllables.java
@@ -1,103 +1,113 @@
 package com.ramonasuncion;
 
-import java.io.*;
+// TODO: A GUI with JavaFX.
+// TODO: Make multiple catch exceptions for FileReading
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// TODO: Eventually I can make a regex that finds all the matches I need to compile once.
+// TODO: Remove the special characters from the string.
+// TODO: Only one word is allowed to be used with the parameter -d
+
 public class Syllables {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         String word = null;
         boolean checkDict = false;
         int syllableCount = 0;
+        int totalCharacters = 0;
+        int totalWords = 0;
 
-        for (String arg : args) {
-            word = arg;
-            System.out.println("Debug arguments: " + word);
+        // Check if there are NO arguments being passed in.
+        if (args.length == 0){
+            System.out.println("usage: Syllables <English word>");
+            System.exit(0);
         }
 
         // Has one argument and gets the syllable count for the words.
-        if (!(args.length == 0)) {
-            word = args[0];
-            syllableCount = getSyllableCount(word);
-        }
+        word = args[0];
+        System.out.println(word);
 
-        // Has more than one argument.
-        if (args.length > 0) {
-            for (String arg : args) {
-                System.out.println(arg);
-                if (arg.equals("-d")) {
-                    checkDict = wordInDictionary(word);
-                }
+        syllableCount = getSyllableCount(word);
+
+        // Check if the word is in the dictionary if the user uses -d.
+        for (String arg : args) {
+            if (arg.equals("-d")) {
+                checkDict = wordInDictionary(word);
+                System.out.println("Is word in dictionary? " + checkDict);
             }
         }
 
         // Debugging variables.
-        System.out.println("Word: " + word);
         System.out.println("Syllable count: " + syllableCount);
-        System.out.println("Is the word in the dict: " + checkDict);
+        System.out.println("Word: " + word);
+        System.out.println("Word count: " + totalWords);
+        System.out.println("Total characters: " + totalCharacters);
     }
 
-    public static int getSyllableCount(String word) {
-        int syllableCount = 0;
-        String lowercaseWord = word.toLowerCase();
-
+    public static int getSyllableCount( String word ) {
         // Checks if there is no words being based in.
         if (wordEmpty(word)) {
             return 0;
         }
 
+        int syllableCount = 0;
+        String lowercaseWord = word.toLowerCase();
+
+        // Check how many vowels are in the words.
         Pattern vowel = Pattern.compile("[aeiouy]+");
         Matcher vowelMatch = vowel.matcher(lowercaseWord);
-        while (vowelMatch.find()){
+        while (vowelMatch.find()) {
             syllableCount++;
         }
 
+        // Check if the word ends with e.
+        Pattern endsWithE = Pattern.compile("e\b");
+        Matcher matchEndsWithE = endsWithE.matcher(lowercaseWord);
+        while (matchEndsWithE.find()) {
+            syllableCount--;
+        }
+
+        // If the word does not start with y.
         Pattern doesNotStartWithY = Pattern.compile("\by");
         Matcher matchRegexForY = doesNotStartWithY.matcher(lowercaseWord);
         while (matchRegexForY.find()) {
             syllableCount++;
         }
 
-        Pattern endsWithE = Pattern.compile("(e\b)");
-        Matcher matchEndsWithE = endsWithE.matcher(lowercaseWord);
-        while (matchEndsWithE.find()) {
-            syllableCount--;
-        }
-
+        // TODO: I can make triphthongs into a regex pattern.
         try {
-            InputStream triphthongs = Syllables.class.getResourceAsStream("/triphthongs.txt");
-            // File triphthongs = new File("/triphthongs.txt");
-            BufferedReader triphthongsFile = new BufferedReader(new InputStreamReader(triphthongs));
-            //BufferedReader triphthongsFile = new BufferedReader(new FileReader(triphthongs));
-
+            BufferedReader triphthongsFile = new BufferedReader(new FileReader("triphthongs.txt"));
             String contentsFromTriphthongsFile;
             while ((contentsFromTriphthongsFile = triphthongsFile.readLine()) != null) {
-                Pattern readFilePattern = Pattern.compile(contentsFromTriphthongsFile);
-                Matcher matchFilePattern = readFilePattern.matcher(lowercaseWord);
-                while (matchFilePattern.find()) {
+
+                // TODO: Change the variable name since already using it below.
+                Pattern triphthongsPattern = Pattern.compile(contentsFromTriphthongsFile);
+                Matcher triphthongsMatch = triphthongsPattern.matcher(lowercaseWord);
+                while (triphthongsMatch.find()) {
                     syllableCount--;
                 }
             }
         } catch (IOException ex) {
-            System.exit(0);
+            System.out.print(ex);
         }
 
         try {
-            InputStream diphthongs = Syllables.class.getResourceAsStream("/diphthongs.txt");
-            BufferedReader diphthongsFile = new BufferedReader(new InputStreamReader(diphthongs));
-
+            BufferedReader diphthongsFile = new BufferedReader(new FileReader("diphthongs.txt"));
             String contentsFromDiphthongsFile;
-
             while ((contentsFromDiphthongsFile = diphthongsFile.readLine()) != null) {
-                Pattern readFilePattern = Pattern.compile(contentsFromDiphthongsFile);
-                Matcher matchFilePattern = readFilePattern.matcher(lowercaseWord);
-                while (matchFilePattern.find()) {
+                Pattern diphthongsPattern = Pattern.compile(contentsFromDiphthongsFile);
+                Matcher diphthongsMatch = diphthongsPattern.matcher(lowercaseWord);
+                while (diphthongsMatch.find()) {
                     syllableCount--;
                 }
             }
         } catch (IOException ex) {
-            System.exit(0);
+            System.out.println(ex);
         }
 
         Pattern le = Pattern.compile("(le\\b)");
@@ -138,32 +148,33 @@ public class Syllables {
             syllableCount++;
         }
 
-        if (syllableCount == 0) {
-            syllableCount++;
+        // If the syllable counter is 0 equal the syllable count to 0.
+        if (syllableCount <= 0) {
+            syllableCount = 1;
         }
 
-        return syllableCount;
+        return (syllableCount);
     }
 
     // Function to check if the word that is being passed into the program is a null or empty.
-    private static boolean wordEmpty(String word) {
+    private static boolean wordEmpty( String word ) {
         return word == null || word.length() == 0;
     }
 
     // Loads up the dictionary txt file into memory and checks if the file contains the word the user passes through
-    public static boolean wordInDictionary(String word) {
+    public static boolean wordInDictionary ( String word ) {
         String noCaseMatter = word.toLowerCase();
         String currentLine;
 
         ArrayList<String> captureDictionary = new ArrayList<>();
 
-        try{
+        try {
             BufferedReader fileReader = new BufferedReader(new FileReader("english.txt"));
             while ((currentLine = fileReader.readLine()) != null) {
                 captureDictionary.add(currentLine);
             }
         } catch (Exception e) {
-            System.out.println("The english.txt file is missing");
+            System.out.println("The english.txt file is missing!");
             System.exit(0);
         }
 
